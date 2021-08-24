1e4727aaf80e6bc6ac1235bdca2aa52c22aa671d
omegamaximus10 <bryan_2904@hotmail.com>
2021-08-24 19:29:40 +0000

    INPROGRESSç

--- src/models.py
+++ src/models.py
@@ -8,8 +8,6 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
@@ -71,7 +69,7 @@ class Favorites(db.Model):
 
     def serialize(self):
         return{
-            "id":self.id,
+            "user_id":self.user.id,
             "people_id":self.people_id,
             "planets_id":self.planets_id,
         }
